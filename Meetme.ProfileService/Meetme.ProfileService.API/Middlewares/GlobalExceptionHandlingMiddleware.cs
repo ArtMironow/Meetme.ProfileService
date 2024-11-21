696a4188402fdc6d1e4d7a530e1fdf6e86afbbb6
ArtMironow <artmironov98@gmail.com>
2024-11-21 12:04:48 +0100

    Add Exception Middleware

---
+++ Meetme.ProfileService/Meetme.ProfileService.API/Middlewares/GlobalExceptionHandlingMiddleware.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using System.Text.Json;
+
+namespace Meetme.ProfileService.API.Middlewares;
+
+public class GlobalExceptionHandlingMiddleware : IMiddleware
+{
+	public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+	{
+		try
+		{
+			await next(context);
+		}
+		catch (Exception)
+		{
+			context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+			ProblemDetails problem = new()
+			{
+				Status = (int)HttpStatusCode.InternalServerError,
+				Title = "Server error",
+				Detail = "An internal server error occured"
+			};
+
+			string json = JsonSerializer.Serialize(problem);
+
+			await context.Response.WriteAsJsonAsync(json);
+		}
+	}
+}
+
