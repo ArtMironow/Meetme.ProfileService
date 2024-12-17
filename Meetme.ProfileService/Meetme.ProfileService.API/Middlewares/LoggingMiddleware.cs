253b4d9e16c4cbd4313fffb46cd612edf02ccdec
ArtMironow <111216832+ArtMironow@users.noreply.github.com>
2024-12-17 14:19:42 +0300

    Add Correlation Id support
    * Add Correlation Id support
    
    * Update Correlation Id support
    
    ---------
    
    Co-authored-by: Mih-Mir <mihailmironov74@gmail.com>

--- Meetme.ProfileService/Meetme.ProfileService.API/Middlewares/LoggingMiddleware.cs
+++ Meetme.ProfileService/Meetme.ProfileService.API/Middlewares/LoggingMiddleware.cs
@@ -1,4 +1,5 @@
-﻿using ILogger = Serilog.ILogger;
+﻿using Serilog.Context;
+using ILogger = Serilog.ILogger;
 
 namespace Meetme.ProfileService.API.Middlewares;
 
@@ -15,12 +16,15 @@ public class LoggingMiddleware
 
 	public async Task InvokeAsync(HttpContext context)
 	{
-		_logger.Information("Handling request: {Method} {Path}",
-			context.Request.Method, context.Request.Path);
+		using (LogContext.PushProperty("CorrelationId", context.TraceIdentifier))
+		{
+			_logger.Information("Handling request: {Method} {Path}",
+				context.Request.Method, context.Request.Path);
 
-		await _next(context);
+			await _next(context);
 
-		_logger.Information("Handled request: {Method} {Path} with {StatusCode}",
-			context.Request.Method, context.Request.Path, context.Response.StatusCode);
+			_logger.Information("Handled request: {Method} {Path} with {StatusCode}",
+				context.Request.Method, context.Request.Path, context.Response.StatusCode);
+		}
 	}
 }
